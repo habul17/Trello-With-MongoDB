ef7dc881c805c9f224997efef53b8ffa89574a39
habul17 <abulhassanca.dev@gmail.com>
2026-07-26 09:01:49 +0530

    Add Signup & Signin routes

--- index.js
+++ index.js
@@ -2,11 +2,71 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const {userModel, organizationModel} = require('./models');
+const { userModel, organizationModel } = require('./models');
 const app = express();
 
 app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userExist = await userModel.findOne({
+        username,
+        password
+    })
+
+    if (userExist) {
+        res.status(403).json({
+            message: "User Already Exist"
+        })
+        return;
+    }
+
+    const newUser = await userModel.create({
+        username,
+        password
+    })
+
+    res.json({
+        message : "Sign Up Successful"
+    })
+
+})
 
 
-app.listen(3000);
+app.post("/signin", async (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userExist = await userModel.findOne({
+        username,
+        password
+    })
+
+    if (!userExist) {
+        res.status(403).json({
+            message: "Invalid Credentials"
+        })
+        return;
+    }
+
+    const token = jwt.sign({userId : userExist._id}, process.env.JWT_SECRET);
+
+    res.json({
+        userId : userExist._id,
+        message : "Sign In Successful",
+        token
+    })
+
+
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+    
+});
